e871cd5b5bccb72edd4aadc0083ebca200d673cb
Julian Mazaira <jmazaira@smarterd.com>
2024-08-28 12:03:09 -0400

    Services Added Mock

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: "#2182BD", // Medium Blue
+    secondary: "#5282BD", // Soft Blue
+    muted: "#C6DAF7", // Light Blue / Pastel Blue
+  },
+  ui: {
+    primary: "#262626", // Dark Charcoal / Almost Black
+    secondary: "#757575", // Grey
+    tertiary: "#F1F1F1", // Light Grey
+    quaternary: "#FFFFFF", // White
+    disabled: "#DEDEDE", // Light Grey (disabled state)
+    error: "#D0421B", // Red (Error color)
+    success: "#138000", // Green (Success color)
+  },
+  bg: {
+    primary: "#FFFFFF", // White
+    secondary: "#F1F1F1", // Light Grey
+  },
+  text: {
+    primary: "#262626", // Dark Charcoal / Almost Black
+    secondary: "#757575", // Grey
+    disabled: "#9C9C9C", // Light Grey (disabled text)
+    inverse: "#FFFFFF", // White (inverse text)
+    error: "#D0421B", // Red (Error text)
+    success: "#138000", // Green (Success text)
+  },
+};
